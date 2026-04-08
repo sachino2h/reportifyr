f68755bbbf0e98a0c3cdf84afcb0e6c341e265ca
Sachin <sachin.patel@o2h.com>
2026-04-08 11:08:42 +0530

    fix: remove test function

--- tests/testthat/test-sum_two_numbers.R
+++
@@ -1,5 +0,0 @@
-test_that("sum_two_numbers returns the sum", {
-  expect_equal(sum_two_numbers(2, 3), 5)
-  expect_equal(sum_two_numbers(-1, 1), 0)
-  expect_equal(sum_two_numbers(0.5, 0.25), 0.75)
-})
